490fb24f1c00bc2d5dc3761cc1eb521f02ac9cf5
samtari1 <samtariyang@gmail.com>
2026-03-05 11:58:42 -0500

    First Commit

--- FoundationModelsExample9GuidedGenerationCustomStruct/ContentView.swift
+++ FoundationModelsExample9GuidedGenerationCustomStruct/ContentView.swift
@@ -6,19 +6,49 @@
 //
 
 import SwiftUI
+import FoundationModels
+
+// Top-level struct for guided generation
+@Generable(description: "An array of the names of the items, lowercase")
+struct ItemNames: Codable {
+    let names: [String]
+}
 
 struct ContentView: View {
+    @State private var response = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Send") {
+                let prompt = "Return only the seven standard colors of the rainbow."
+                let instructions = """
+                Return an array of items
+                """
+                
+                let session = LanguageModelSession(instructions: instructions)
+            
+                if !session.isResponding {
+                    Task {
+                        do {
+                            let answer = try await session.respond(
+                                to: prompt,
+                                generating: ItemNames.self,
+                            )
+                            response = answer.content.names.joined(separator: ", ")
+                        } catch {
+                            response = "Error accessing the model: \(error)"
+                        }
+                    }
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Text(response)
+                .font(.system(size: 18))
+                .padding()
+            
+            Spacer()
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
